b38efcb74830e9f8a564e97da7ae8814dc21acd2
Criss Allison <criss.allison@gmail.com>
2011-10-01 17:13:06 -0400

    Tardis Library Creation. 3 files created - html + 2.js created factory return init function
    Signed-off-by: Criss Allison <criss.allison@gmail.com>

---
+++ tardis.js
@@ -0,0 +1,14 @@
+// Tardis JS library
+// Created by Chris Allison
+
+var tardis = function(){
+	return new tardis.prototype.init();
+};
+
+tardis.prototype = {
+	init: function(){
+		//init the library	
+	}
+};
+
+tardis.prototype.init.prototype = tardis.prototype;
