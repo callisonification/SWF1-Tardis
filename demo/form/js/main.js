77201bb67f231298d211269c1c123e1e0e92be12
Criss Allison <criss.allison@gmail.com>
2011-10-08 16:27:59 -0400

    Added AJAX module(s)
    Signed-off-by: Criss Allison <criss.allison@gmail.com>

--- demo/form/js/main.js
+++ demo/form/js/main.js
@@ -21,3 +21,51 @@
 	
 	tardis("div").addClass("testing");
 })();
+
+///homework code below///
+
+(function(){
+	var validationSet = {
+		//holds other objects, each object is a validation type;
+		email: {
+			test: function(elem){
+				var pattern = /pattern/;
+				return pattern.test(elem.value);	
+			},
+			message: "Invalid email address"	
+		},
+		phone: {
+			test: function(){},
+			message: "Invalid email address"	
+		},
+		text: {
+			test: function(){},
+			message: "Invalid email address"	
+		},
+		password: {
+			test: function(){},
+			message: "Invalid email address"	
+		},
+		website: {
+			test: function(){},
+			message: "Invalid email address"	
+		}
+	};	
+	
+	var regFields = tardis("#myform input")
+	
+	regFields.each(function(){
+		this.onkeyup = function(){
+			for(var key in validationSet){
+				if( tardis(this).hasClass(key) ){
+					if( validationSet[key].test(this) ){
+						console.log("test fn ran");
+					}else{
+						console.log("no test ran");
+					};
+				};
+			};
+		};
+	});
+	
+})();
--- tardis.js
+++ tardis.js
@@ -14,6 +14,7 @@ tardis.prototype = {
 			this.elements = [selector];
 		};
 	},
+	
 	elements: [],
 	
 	each: function(fn){
@@ -22,6 +23,7 @@ tardis.prototype = {
 		};
 		return this;
 	},
+	
 	css: function(props){
 		for(var prop in props) {
 			this.each(function(){
@@ -55,6 +57,71 @@ tardis.prototype = {
 			this.className = this.className.replace(pattern, "$1").replace(/ $/, "");
 		});	
 	}
+};
+
+//Library utility functions - non-Dom methods
+/*
+	tardis.ajax({				< pass object literal into options argument
+		url: "xhr/file.php", 	//example URL
+		type: "GET",			//mostly use GET must be in all caps
+		success: function(response){},
+		error: function(response){},
+		timeout: 8000			//sets timeout to 8 seconds
+	}); 
+*/
+tardis.ajax = function(options){
+	
+	options = {
+		url: options.url || "",
+		type: options.type || "GET",
+		timeout: options.timeout || 8000,
+		success: options.success || function(){},
+		error: options.error || function(){}
+	};
+	
+	setTimeout(function(){
+		if(xhr){
+			xhr.abort();			
+		}
+	}, options.timeOut);
+	
+	var checkHttp = function(){
+		try{
+			return !xhr.status && location.protocol === "file:" || 
+				( xhr.status >= 200 && xhr.status <300 ) ||
+				xhr.status === 304 ||
+				navigator.userAgent.indexOf("Safari") >= 0 && xhr.status === "undefined"
+			;
+		}catch(err){};
+		
+		return false;
+	};
+	
+	var parseData = function(){
+		var ct = xhr.getResponseHeader("content-type");
+		var isxml = ct && ct.indexOf("xml")	>= 0;
+		return isxml ? xhr.responseXML : xhr.responseText;
+	};
+	
+	var serialize = function(){};
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open(options.type, options.url, true);
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4){
+			var valid = checkHttp();
+			if(valid){
+				var response = parseData()
+				options.success( response )
+			}else{
+				//fail
+				options.error(xhr);
+			};
+			xhr = undefined;
+		};
+	};
+	xhr.send(null);
+	
 };
 
 tardis.prototype.init.prototype = tardis.prototype;
